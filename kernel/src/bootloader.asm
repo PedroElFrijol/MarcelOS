d37e214b2937b52d39b40b50a1dcba5da18196de
Marcelo Rios <marceloraulriosjr@gmail.com>
2022-12-21 17:52:06 -0700

    Update bootsector

--- kernel/src/bootloader.asm
+++ kernel/src/bootloader.asm
@@ -1,5 +1,6 @@
 [bits 16] ;computer starts in Real Mode which is 16 bit
 [org 0x7C00] ;tells the compiler where the code will be (origin)
+;each interrupt requires certain data to be stored in certain registers.
 
 ; Set video mode
 mov ah, 0x00
@@ -22,9 +23,12 @@ sti ;enabling interrupts
 mov [BOOT_DISK], dl ;the disk we are trying to read is most likely the disk we booted from (aka "dl")
 
 ; Read in sectors
-mov ax, kernelLocation
+mov ax, kernel_loc
 mov es, ax          
-xor bx, bx          ; bx=0, bx:es=0x0:0x07E0=0x7E00
+xor bx, bx  ; segment:offset = es:bx
+; formula = (segment * 16) + offset
+; es:bx = 0x07E0(kernel_loc):0x0000
+; (0x07E0 * 16) + 0x0000 = 0x7E00
 
 mov ah, 2
 mov al, 1 ;number of sectors we want to read which is 1
@@ -33,7 +37,7 @@ mov dh, 0 ;head number which equals 0
 mov cl, 2 ;the sector number which equals 2 because we are reading from the second sector of the same head and the same cyilnder
 mov dl, [BOOT_DISK] ;drive number we saved in the variable
 int 0x13 ;or 13h for disk access
-;jc failed
+jc failed ;jump to failed label if something goes wrong
 
 ;Enabling the A20 Line
 mov al, 0x92
@@ -54,6 +58,31 @@ jmp $
 
 %include "src/gdt.asm"
 
+failed:
+  mov ah, 0x0E
+  mov al, 'E'
+  int 0x10
+.read_failed_hlt:
+  jmp .read_failed_hlt
+
+print:
+  mov ah, 0x0E    ; BIOS teletyping function
+
+.print_loop:      ;loop until string is null terminated (\0)
+  mov al, [si]    ;get current character
+  cmp al, 0x0     ;is `al` 0(null terminated)?
+  je .end_print   ;if yes, loop no more and return
+
+  ; `al` stores the character to print, so if it isn't 0(\0), its a valid character.
+  ; invoke `int 0x10` to print character via `AH=0x0E`
+  int 0x10 ;requires the character to be printed to be stored in ah
+
+  ; increment the address of `si` to get next character
+  inc si
+  ; continue the loop until we reach 0x0(\0)
+  jmp .print_loop
+    ret
+
 [bits 32]
 beginProtectedMode:
     mov ax, dataSegment
@@ -66,17 +95,11 @@ beginProtectedMode:
     mov si, text ;point text to source index
     call print               
 
-    text db 'Welcome to MarcelOS!',0
-
-    print:
-        mov ah, 0x0E
-
     jmp codeSegment:kernelLocation
-    jmp codeSegment:kernel_exact
 
 BOOT_DISK db 0
-kernelLocation equ 0x07E0 ; segment:offset = 0x0:0x07E0=0x7E00
-kernel_exact equ 0x7c00 + 0x200 ; actual address
+kernelLocation equ 0x7c00 + 0x200 ; actual address
+text db 'Welcome to MarcelOS!',0
 
 times 510 - ($ - $$) db 0x0
 dw 0xAA55
